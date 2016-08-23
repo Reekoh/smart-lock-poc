baa5836b6aa0359a7f93a5d3cec257285ca51fc0
Lawrence <l.marcelino007@gmail.com>
2016-08-23 22:35:24 +0800

    Integrate UI to ionic functionality

--- app/app.ts
+++ app/app.ts
@@ -1,31 +1,33 @@
 import {Component} from '@angular/core';
-import {Platform, MenuController, ionicBootstrap} from 'ionic-angular';
-import {StatusBar} from 'ionic-native';
+import {Platform, MenuController, ionicBootstrap, AlertController} from 'ionic-angular';
+import {StatusBar, SpinnerDialog} from 'ionic-native';
 import {HomePage} from './pages/home/home';
 import {RegistrationPage} from './pages/registration/registration';
 import {AboutPage} from './pages/about/about';
+import {AccountService} from './shared/account.service';
 
 @Component({
+  providers: [AccountService],
   template: `
     <ion-menu class="menu-nav" [content]="content">
       <ion-toolbar>
         <ion-title>
         Menu
-        <img class="back-ico" src="../../images/left-icon.png">
+        <img class="back-ico" src="images/left-icon.png" (click)="closeMenu()">
         </ion-title>
       </ion-toolbar>
       <ion-content>
         <ion-list>
           <button ion-item (click)="openPage(homePage)">
-            <img class="img-icon" src="../../images/home-icon.png" >
+            <img class="img-icon" src="images/home-icon.png" >
             Home
           </button>
           <button ion-item (click)="openPage(registrationPage)">
-            <img class="img-icon" src="../../images/register-icon.png" >
+            <img class="img-icon" src="images/register-icon.png" >
             Register Profile
           </button>
           <button ion-item (click)="openPage(aboutPage)">
-            <img class="img-icon" src="../../images/about-icon.png" >
+            <img class="img-icon" src="images/about-icon.png" >
             About
           </button>
         </ion-list>
@@ -42,18 +44,36 @@ export class MyApp {
   private registrationPage = RegistrationPage;
   private aboutPage = AboutPage;
 
-  constructor(private platform: Platform, private menu: MenuController) {
+  constructor(private platform: Platform, private menu: MenuController, private alertController: AlertController) {
     this.rootPage = HomePage;
 
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
+      let socket = new WebSocket('ws://demo1.reekoh.com:8071');
+		  socket.onopen = (event) => {
+        socket.onmessage = (event) =>{
+          let result = JSON.parse(event.data);
+          let subTitle = result.match_found && result.match_found[0].candidates.length > 0 ? 'Face successfully verified' : 'Can\'t verified, please try again';
+          let alert = this.alertController.create({
+                      title: 'Face Identification',
+                      subTitle: subTitle,
+                      buttons: ['OK']
+            });
+            alert.present();
+            SpinnerDialog.hide();
+          }
+      }
     });
   }
 
   openPage(page) {
     this.rootPage = page;
+    this.menu.close();
+  }
+
+  closeMenu() {
     this.menu.close();
   }
 }
--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,11 +1,80 @@
 import {Component} from '@angular/core';
-import {NavController} from 'ionic-angular';
+import {Platform, LoadingController} from 'ionic-angular';
+import {AccountService} from '../../shared/account.service';
+import { SpinnerDialog } from 'ionic-native';
 
+declare var navigator:any;
+declare var cordova;
+declare var window;
 @Component({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
-  constructor(private navCtrl: NavController) {
-  
+  constructor(private platform: Platform, private accountService: AccountService) {
+    platform.ready().then(() => {
+      var self = this;
+      var permissions = cordova.plugins.permissions;
+      permissions.hasPermission(permissions.CAMERA, checkPermissionCallback, null);
+      
+      function checkPermissionCallback(status) {
+        if(!status.hasPermission) {
+          var errorCallback = function() {
+            console.warn('Camera permission is not turned on');
+          }
+      
+          permissions.requestPermission(
+            permissions.CAMERA,
+            (status) => {
+              if(!status.hasPermission) errorCallback();
+              else {
+                self.showPreviewCamera();              
+              }
+            },
+            errorCallback);
+        } else {
+          self.showPreviewCamera();
+        }
+      }
+    });
+  }
+
+  showPreviewCamera() {
+    let tapEnabled = false;
+    let dragEnabled = false;
+    let toBack = true;
+    let rect = {
+        x: 0, 
+        y: 0, 
+        width: this.platform.width(), 
+        height: this.platform.height()
+    };
+    cordova.plugins.camerapreview.startCamera(rect, "front", tapEnabled, dragEnabled, toBack);
+  }
+  validate() {
+    SpinnerDialog.show();
+    let self = this;
+    cordova.plugins.camerapreview.takePicture({maxWidth:640, maxHeight:640});
+    cordova.plugins.camerapreview.setOnPictureTakenHandler(function(result){
+      console.log(result[0]);
+
+      window.resolveLocalFileSystemURL('file://' + result[1], gotFile, fail);
+            
+      function fail(e) {
+            alert('Cannot found requested file');
+      }
+
+      function gotFile(fileEntry) {
+            fileEntry.file((file) => {
+                var reader = new FileReader();
+                reader.onloadend = function(e) {
+                    var content = this.result.split(',')[1];
+                    console.log(content)
+                    self.accountService.validateImage(content);
+                };
+                // The most important point, use the readAsDatURL Method from the file plugin
+                reader.readAsDataURL(file);
+            });
+      }
+    });
   }
 }
